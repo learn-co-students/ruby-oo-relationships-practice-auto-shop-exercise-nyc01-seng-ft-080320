1fb567370ea35f8ffae35f9307326d5ad6c081c6
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-09 22:06:11 -0400

    completed lab

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,36 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select do |car|
+      car.car_owner == self
+    end
+  end
+
+  def mechanics
+    self.cars.map do |car|
+      car.mechanic
+    end
+  end
+
+  def self.average_number_of_cars
+    owned = CarOwner.all.map do |car_owner|
+      car_owner.cars.count
+    end
+    length = owned.length.to_f
+    owned = owned.sum.to_f
+    owned/length
+  end
 
 end
